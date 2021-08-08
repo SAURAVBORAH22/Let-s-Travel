25b9207790ba30c6252039661e6296dee8aaea53
unknown <SAURAVBORAH22>
2021-08-09 01:27:47 +0530

    nodejs REST API

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+//calling express
+const express = require("express");
+//calling mongoose
+const mongoose = require("mongoose");
+//calling .env file
+const dotenv = require("dotenv");
+//calling our app
+const app = express();
+///calling the pins file
+const pinRoute = require("./routes/pins");
+
+//configuring the dotenv
+dotenv.config();
+
+//using json
+app.use(express.json())
+
+//connecting mongodb 
+// go to https://mongoosejs.com/docs/connections.html
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true, //allow users to fall back to the old parser if they find a bug in the new parser
+    useUnifiedTopology: true, //unified topology
+})
+    .then(() => { //if connected to the mongodb url
+        console.log("MongoDB Connected!")
+    }).catch((err) => console.log(err)); //if error then we are just console loging
+
+
+//defining the route
+app.use("/api/pins", pinRoute);
+
+//deciding the port
+app.listen(8800, () => {
+    console.log("Backend server is running");
+});
---
+++ backend/routes/pins.js
@@ -0,0 +1,31 @@
+//calling the router
+const router = require("express").Router();
+
+///calling pin 
+const Pin = require("../models/Pin");
+
+//create pin
+router.post("/", async (req, res) => { //passing request and response as a async function
+    const newPin = new Pin(req.body);
+    try {
+        //awaiting the function
+        const savedPin = await newPin.save();//else saving the pin
+        res.status(200).json(savedPin);//passing the status code 200 for successful and savedPin in json format
+    } catch (err) {
+        res.status(500).json(err); //if error is caught then we are passing the statuscode and the err in json format
+    }
+});
+
+
+//get all pins
+router.get("/", async (req, res) => {
+    try {
+        const pins = await Pin.find(); //finding all the pins in our model
+        res.status(200).json(pins);//passing the status code 200 for successful and pins in json format
+    } catch (err) {
+        res.status(500).json(err); //if error is caught then we are passing the statuscode and the err in json format
+    }
+});
+
+//exporting pins model
+module.exports = router
