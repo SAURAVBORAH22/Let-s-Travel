1b49b4004b869b2d019fb6da347925aef76708af
SAURAVBORAH22 <sauravborah1999@gmail.com>
2021-08-11 01:50:36 +0530

    react fetching data from node.js using axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 //importing the usestate hook
 //the usestate allows us to add a state to our fucntional components
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 //importing the ReactMapGl , marker 
 import ReactMapGL, { Marker, Popup } from 'react-map-gl';
@@ -11,7 +11,22 @@ import { Room, Star } from '@material-ui/icons';
 //importing the app.css file
 import "./app.css";
 
+//importing axios
+import axios from "axios";
+
+//importing format
+import { format } from "timeago.js";
+
 function App() {
+    //temporary current user
+    const currentUser = "SB"
+
+    //creating a usestate hook for setting and using pins
+    const [pins, setPins] = useState([]);
+
+    //creating another useState hook for setting currentplaceid
+    const [currentPlaceId, setCurrentPlaceId] = useState(null);
+
     //the usestate hook defining and seting the viewport
     //https://visgl.github.io/react-map-gl/docs/get-started/get-started
     const [viewport, setViewport] = useState({
@@ -21,6 +36,27 @@ function App() {
         longitude: 78.9629,
         zoom: 4 //zooming in the map
     });
+
+    //useeffect tells the react component what it need to do after render
+    useEffect(() => {
+        //creating a async function getPins to send a get all the pins
+        const getPins = async () => {
+            try {
+                //axios will allow use to connect to our api in out backend
+                const res = await axios.get("/pins");
+                setPins(res.data);//setting the pins with all data inside from the db
+            } catch (err) {
+                console.log(err);//logging the error
+            }
+        };
+        getPins();
+    }, []//empty array because we are gonna fire the useffect at the begining
+    );
+
+    const handleMarkerClick = (id) => {
+        setCurrentPlaceId(id);
+    };
+
     return (
         <div className="App">
             <ReactMapGL
@@ -30,40 +66,49 @@ function App() {
                 onViewportChange={nextViewport => setViewport(nextViewport)}
                 mapStyle="mapbox://styles/safak/cknndpyfq268f17p53nmpwira" //this map style i found on internet seemed much better than the traditional grey one
             >
-                <Marker //marker :-https://visgl.github.io/react-map-gl/docs/api-reference/marker
-                    latitude={27.1751} //latitude of marker
-                    longitude={78.0421} //longitutde of marker
-                    offsetLeft={-20}
-                    offsetTop={-10}>
-                    <Room style={{ fontSize: viewport.zoom * 7, color: 'slateblue' }} />
-                </Marker>
-                {/* <Popup //popup:- https://visgl.github.io/react-map-gl/docs/api-reference/popup
-                    latitude={27.1751} //latitude of the popup
-                    longitude={78.0421} //logitutde of the popup
-                    closeButton={true} //providing close button
-                    closeOnClick={false} //whether to close or not when clicked close button
-                    anchor="left" //the anchor position  
-                >
-                    <div className="card">
-                        <label>Place</label>
-                        <h4 className="place">Taj Mahal</h4>
-                        <label>Review</label>
-                        <p className="desc">Beautiful place. I like it.</p>
-                        <label>Rating</label>
-                        <div className="stars">
-                            <Star className="star" />
-                            <Star className="star" />
-                            <Star className="star" />
-                            <Star className="star" />
-                            <Star className="star" />
-                        </div>
-                        <label>Information </label>
-                        <span className="username">Created by <b>Saurav</b> </span>
-                        <span className="date">1 hour ago </span>
-                    </div>
-                </Popup> */}
+                {pins.map(p => ( //for every pin we are defining the below properties
+                    <>
+                        <Marker //marker :-https://visgl.github.io/react-map-gl/docs/api-reference/marker
+                            latitude={p.lat} //latitude of marker
+                            longitude={p.long} //longitutde of marker
+                            offsetLeft={-20}
+                            offsetTop={-10}>
+                            <Room style={{ fontSize: viewport.zoom * 7, color: p.username===currentUser?"tomato":"slateblue", cursor:"pointer" }}
+                                onClick={() => handleMarkerClick(p._id)} //function to handle the marker on click
+                            />
+                        </Marker>
+                        {p._id === currentPlaceId && ( //if pin id is equal to the currentplace id then we can open this popup
+                            < Popup //popup:- https://visgl.github.io/react-map-gl/docs/api-reference/popup
+                                latitude={p.lat} //latitude of the popup
+                                longitude={p.long} //logitutde of the popup
+                                closeButton={true} //providing close button
+                                closeOnClick={false} //whether to close or not when clicked close button
+                                anchor="left" //the anchor position  
+                                onClose={()=>setCurrentPlaceId(null)} //closing the popup
+                            >
+                                <div className="card">
+                                    <label>Place</label>
+                                    <h4 className="place">{p.title}</h4>
+                                    <label>Review</label>
+                                    <p className="desc">{p.desc}</p>
+                                    <label>Rating</label>
+                                    <div className="stars">
+                                        <Star className="star" />
+                                        <Star className="star" />
+                                        <Star className="star" />
+                                        <Star className="star" />
+                                        <Star className="star" />
+                                    </div>
+                                    <label>Information </label>
+                                    <span className="username">Created by <b>{p.username}</b> </span>
+                                    <span className="date">{format(p.createdAt)} </span>
+                                </div>
+                            </Popup>
+                        )}
+                    </>
+                ))}
             </ReactMapGL>
-        </div>
+        </div >
     );
 }
 
