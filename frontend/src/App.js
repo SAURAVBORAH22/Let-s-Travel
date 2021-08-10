db94278156976348e191ac7f4c1f722e6b17c843
SAURAVBORAH22 <sauravborah1999@gmail.com>
2021-08-10 12:14:47 +0530

    adding marker

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,11 @@
 //the usestate allows us to add a state to our fucntional components
 import { useState } from 'react';
 
-//imporrting the ReactMapGl 
-import ReactMapGL from 'react-map-gl';
+//importing the ReactMapGl , marker 
+import ReactMapGL, { Marker } from 'react-map-gl';
+
+//importing the room marker
+import { Room } from '@material-ui/icons';
 
 function App() {
     //the usestate hook defining and seting the viewport
@@ -22,7 +25,16 @@ function App() {
                 //providing the max access token for the api
                 mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
                 onViewportChange={nextViewport => setViewport(nextViewport)}
-            />
+                mapStyle="mapbox://styles/safak/cknndpyfq268f17p53nmpwira" //this map style i found on internet seemed much better than the traditional grey one
+            >
+                <Marker //marker :-https://visgl.github.io/react-map-gl/docs/api-reference/marker
+                    latitude={27.1751} //latitude of marker
+                    longitude={78.0421} //longitutde of marker
+                    offsetLeft={-20}
+                    offsetTop={-10}>
+                    <Room style={{ fontSize: viewport.zoom * 7, color: 'slateblue' }} />
+                </Marker>
+            </ReactMapGL>
         </div>
     );
 }
